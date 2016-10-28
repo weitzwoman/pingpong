fa6d4ad5fe397dce79c3b376c5ae612ba8a38c56
Tracie Weitzman <tlweitzman@gmail.com>
2016-10-28 10:31:21 -0700

    add JS functions to identify if user input number is divisible by 15, 3, 5, if not, pass through numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,40 @@
 //business
+var userResult = [];
 
 function hasNumbers(originalInput) {
   return /\d/.test(originalInput);
 }
 
-
-
-
+function pingPong(originalInput) {
+  for (var index = 1; index <= originalInput; index++) {
+    if ((index % 15) === 0) {
+      userResult.push("pingpong!");
+    } else if ((index % 3) === 0) {
+      userResult.push("ping!");
+    } else if ((index % 5) === 0) {
+      userResult.push("pong!");
+    } else {
+      userResult.push(index);
+    }
+  } return userResult;
+};
 
 // UI
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
-    event.preventDefault();
 
     var originalInput = parseInt($("input#userInput").val());
     var isNumber = hasNumbers(originalInput);
-    // var finalOutput = _____(originalInput);
+    var finalOutput = pingPong(originalInput);
 
     if (isNumber === false) {
+      alert("Please only use numbers");
+    } else if (originalInput < 1) {
       alert("Please only use positive numbers");
     } else {
-      $("result").text(finalOutput);
-      $("output").append();
+      $("#result").text(finalOutput);
+      $("#output").show();
     }
-
+    event.preventDefault();
   });
 });
